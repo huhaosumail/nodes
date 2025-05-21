b06a2f7fce34b408d26fd848e5f8ae0dcafe8c7e
huhaosumail <995483610@qq.com>
2025-05-21 20:39:19 +0800

    feat(algorithm): 实现二叉树的Z字形层序遍历算法，支持从左到右和从右到左的交替遍历

--- algorithm/src/main/java/com/hh/algorithm/ZigzagLevelOrder.java
+++ algorithm/src/main/java/com/hh/algorithm/ZigzagLevelOrder.java
@@ -1,5 +1,7 @@
 package com.hh.algorithm;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -15,6 +17,63 @@ public class ZigzagLevelOrder {
 
     public static List<List<Integer>> zigzagLevelOrder(LevelOrder.TreeNode root) {
 
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        // 利用栈实现层序遍历
+        LinkedList<LevelOrder.TreeNode> stack = new LinkedList<>();
+        stack.addFirst(root);
+
+        // true：表示从左往右
+        // false：表示从右往左
+        boolean flag = true;
+        while (!stack.isEmpty()) {
+            // 因为每一层都需要放在一个集合中
+            // 所以这里先把同一层的节点放到list中
+            List<LevelOrder.TreeNode> list = new ArrayList<>();
+            // 遍历队列
+            while (!stack.isEmpty()) {
+                // 将栈中的元素放入list中
+                // 这里就是将同一层到节点放入到list中
+                list.add(stack.removeFirst());
+            }
+
+            List<Integer> nums = new ArrayList<>();
+            // 然后再遍历这一层的节点
+            for (LevelOrder.TreeNode node : list) {
+                // 当前节点值放入nums集合中
+                nums.add(node.val);
+
+                if (flag) { // 如果当前是从左往右
+                    // 则先将left入栈
+                    if (node.left != null) {
+                        stack.addFirst(node.left);
+                    }
+                    // 再将right入栈
+                    if (node.right != null) {
+                        stack.addFirst(node.right);
+                    }
+                } else { // 如果当前是从右往左
+                    // 则先将right入栈
+                    if (node.right != null) {
+                        stack.addFirst(node.right);
+                    }
+                    // 再将left入栈
+                    if (node.left != null) {
+                        stack.addFirst(node.left);
+                    }
+                }
+            }
+
+            // 变换方向
+            flag = !flag;
+            // 将当前层的结果放入res中
+            res.add(nums);
+        }
+
+        return res;
 
 
     }
