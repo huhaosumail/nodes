b5f28bb5d12050c97b68272411685b5ea6f294b4
huhaosumail <995483610@qq.com>
2025-05-23 14:29:02 +0800

    feat(algorithm): 接雨水

---
+++ algorithm/src/main/java/com/hh/algorithm/InorderTraversal.java
@@ -0,0 +1,17 @@
+package com.hh.algorithm;
+
+/**
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ *
+ * 提示：
+ *
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ */
+public class InorderTraversal {
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- algorithm/src/main/java/com/hh/algorithm/Trap.java
+++ algorithm/src/main/java/com/hh/algorithm/Trap.java
@@ -12,6 +12,33 @@ package com.hh.algorithm;
 public class Trap {
 
 
+    public int trap(int[] height) {
+        int length;
+        if ((length = height.length) == 0) {
+            return 0;
+        }
 
+        // 每个柱子能接多少雨水等于当前柱子两边最大高度的较小值减去当前高度的值
+        int sum = 0, left, right;
+        for (int i = 1; i < length; i++) {
+            left = right = height[i];
+
+            // 找到左边最大最大高度的柱子
+            for (int j = i - 1; j >= 0; j--) {
+                left = Math.max(left, height[j]);
+            }
+
+            // 找到右边最大最大高度的柱子
+            for (int j = i + 1; j < length; j++) {
+                right = Math.max(right, height[j]);
+            }
+
+            // 当前柱子两边最大高度的较小值减去当前高度的值
+            // 就是当前柱子能接的雨水
+            sum += Math.min(left, right) - height[i];
+        }
+
+        return sum;
+    }
 
 }
