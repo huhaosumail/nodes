17cc732802b43dd4c4e4e9d20bc0173c58aa40d2
huhaosumail <995483610@qq.com>
2025-05-26 14:24:10 +0800

    feat(algorithm): 最长公共子序列

--- algorithm/src/main/java/com/hh/algorithm/LongestCommonSubsequence.java
+++ algorithm/src/main/java/com/hh/algorithm/LongestCommonSubsequence.java
@@ -14,7 +14,31 @@ package com.hh.algorithm;
  */
 public class LongestCommonSubsequence {
 
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        // 利用二维数组dp保存两个字符串公共子序列的长度
+        // 其中dp[i][j]表示text1[0:i]和text2[0:j]的最长公共子序列的长度
+        int[][] dp = new int[m + 1][n + 1];
 
+        for (int i = 1; i <= m; i++) {
+            char c1 = text1.charAt(i - 1);
+            for (int j = 1; j <= n; j++) {
+                char c2 = text2.charAt(j - 1);
+                if (c1 == c2) {
+                    // 当前c1 == c2时
+                    // 要考虑text1[0:i-1]和text2[0:j-1]的公共子序列
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    // 否则，需要考虑text1[0:i-1]和text2[0:j]的公共子序列
+                    // 以及text1[0:i]和text2[0:j-1]的公共子序列
+                    // 并取两者的最大值
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
     public static void main(String[] args) {
 
     }
