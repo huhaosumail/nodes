99fe48edbaf9a851bde332628f876a937fa9a95c
huhaosumail <995483610@qq.com>
2025-05-30 22:22:32 +0800

    feat(algorithm): 公共祖先和lru

--- algorithm/src/main/java/com/hh/algorithm/LRUCache.java
+++ algorithm/src/main/java/com/hh/algorithm/LRUCache.java
@@ -10,14 +10,12 @@ import java.util.Map;
  * 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 void put(int key, int value)
  * 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。
  * 如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 1 <= capacity <= 3000 0 <= key <= 10000 0 <= value <= 105 最多调用 2 * 105 次 get 和 put
  */
 public class LRUCache {
-
-
     private int capacity;
     // memory维护key和Node的关系
     private Map<Integer, Node> memory;
@@ -37,57 +35,45 @@ public class LRUCache {
         }
     }
 
-    /**
-     * 初始化
-     */
     public LRUCache(int capacity) {
-        this.capacity = capacity;
         this.memory = new HashMap<>();
         this.linkedList = new LinkedList<>();
+        this.capacity = capacity;
     }
 
     public int get(int key) {
-        // key不存在就返回-1
         if (!memory.containsKey(key)) {
             return -1;
         }
-
-        // key存在获取对应Node
         Node node = memory.get(key);
-
-        // 将Node调整到链表的最前面
+        //因为有访问 所以重新放在第一个
         linkedList.remove(node);
         linkedList.addFirst(node);
-
         return node.value;
     }
 
     public void put(int key, int value) {
-        // key已经存在
+
         if (memory.containsKey(key)) {
+            //这里长度不会变大
             // 获取对应Node
             Node node = memory.get(key);
             // 更新value
             node.value = value;
-
-            // 将Node调整到链表的最前面
+            //因为有访问 所以重新放在第一个
             linkedList.remove(node);
             linkedList.addFirst(node);
-
-            return ;
+            return;
         }
 
-        // 如果已经满了
         if (memory.size() == capacity) {
-            // 就删除最后一个Node
+            //就删除最后一个Node 保证长度先符合规范
             Node node = linkedList.removeLast();
             memory.remove(node.key);
         }
-
-        // 将新的key和value放到链表的最前面
-        Node node = new Node(key, value);
+        //存入新的node
+        Node node =new Node(key,value);
+        memory.put(key,node);
         linkedList.addFirst(node);
-        memory.put(key, node);
     }
-
 }
