0f67761dc49c2b8ec1d7dd55d7835437315843b4
huhaosumail <995483610@qq.com>
2025-06-04 11:39:32 +0800

    feat(algorithm): final

--- algorithm/src/main/java/com/hh/algorithm/ReverseKGroup.java
+++ algorithm/src/main/java/com/hh/algorithm/ReverseKGroup.java
@@ -2,17 +2,17 @@ package com.hh.algorithm;
 
 /**
  * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
- *
+ * <p>
  * k 是一个正整数，它的值小于或等于链表的长度。
- *
+ * <p>
  * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
- *
+ * <p>
  * 进阶：
- *
+ * <p>
  * 你可以设计一个只使用常数额外空间的算法来解决此问题吗？ 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 列表中节点的数量在范围 sz 内
  * 1 <= sz <= 5000
  * 0 <= Node.val <= 1000
@@ -20,12 +20,18 @@ package com.hh.algorithm;
  */
 public class ReverseKGroup {
 
-    public static class ListNode{
+    public static class ListNode {
         public int val;
         public ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        public ListNode(int val,ListNode next){
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
@@ -33,61 +39,64 @@ public class ReverseKGroup {
 
     public static ListNode reverseKGroup(ListNode head, int k) {
         if (head == null) {
-            return head;
+            return null;
         }
-
         ListNode node = head;
         int i = 0;
-        // 先将链表按k个节点进行分割
         while (node != null && ++i < k) {
             node = node.next;
         }
-
-        // 如果剩余链表长度小于k
         if (i < k) {
-            // 就直接返回头节点head
             return head;
         }
-
-        // 分割链表
+        //先保存后续临时节点 同时下一步进行断琏
         ListNode next = node.next;
         node.next = null;
 
-        // 反转当前段的链表
+        //反转前k个链表
         ListNode newHead = reverseListNode(head);
-        // 然后继续递归反转剩余的链表
+        //将临时节点带入后续再进行反转 head.next是精髓 串联k个节点组用的
         head.next = reverseKGroup(next, k);
-
         return newHead;
     }
 
-    /**
-     * 反转链表
-     */
     private static ListNode reverseListNode(ListNode head) {
-        if (head == null || head.next == null) {
-            return head;
+        if (head == null) {
+            return null;
         }
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next;
+        while (curr != null) {
+//              //临时节点，暂存当前节点的下一节点，用于后移
+//              //因为接下来curr.next进行赋值后将会断链 这里最主要的是断琏 并保存
+//              next = curr.next;
+//              //将当前节点指向它前面的节点
+//              curr.next = prev;
+//              //前指针后移
+//              prev = curr;
+//              //当前指针后移
+//              curr = next;
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
 
-        ListNode next = head.next;
-        ListNode node = reverseListNode(next);
-
-        head.next = null;
-        next.next = head;
-
-        return node;
+        }
+        return prev;
     }
 
+
     public static void main(String[] args) {
-        ListNode  node5 = new ListNode(5,null);
-        ListNode  node4 = new ListNode(4,node5);
-        ListNode  node3 = new ListNode(3,node4);
-        ListNode  node2 = new ListNode(2,node3);
-        ListNode  node1 = new ListNode(1,node2);
+        ListNode node5 = new ListNode(5, null);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
 
-        ListNode  result = reverseKGroup(node1,3);
+        ListNode result = reverseKGroup(node1, 3);
 
-        while(result != null){
+        while (result != null) {
             System.out.println(result.val);
             result = result.next;
         }
--- algorithm/src/main/java/com/hh/algorithm/ThreeSum.java
+++ algorithm/src/main/java/com/hh/algorithm/ThreeSum.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 /**
  * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
- *
+ * <p>
  * 注意：答案中不可以包含重复的三元组。
- *
+ * <p>
  * 提示：
- *
+ * <p>
  * 0 <= nums.length <= 3000
  * -105 <= nums[i] <= 105
  */
@@ -21,59 +21,49 @@ import java.util.List;
 public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
+
         List<List<Integer>> res = new ArrayList<>();
-        int length;
-        if ((length = nums.length) == 0) {
+
+        int length = nums.length;
+        if (length == 0) {
             return res;
         }
-
-        // 先对数组nums进行排序
         Arrays.sort(nums);
 
-        // 从第一个元素开始
-        // 将当前元素和后面元素一头一尾相加
         for (int i = 0; i < length - 2; i++) {
             // 跳过重复的元素
-            if (i > 0 && nums[i - 1] == nums[i]) {
-                continue ;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
             }
+            int left = i + 1;
+            int right = length - 1;
+            while (left < right) {
 
-            // l和r之间形成一个区间
-            int l = i + 1, r = length - 1;
-            while (l < r) {
-                int num = nums[i] + nums[l] + nums[r];
+                int num = nums[i] + nums[left] + nums[right];
                 if (num == 0) {
-                    // 如果结果等于0
-                    // 则记录结果
                     List<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
-                    list.add(nums[l]);
-                    list.add(nums[r]);
-
-                    // 并将结果加入res
+                    list.add(nums[left]);
+                    list.add(nums[right]);
                     res.add(list);
-
-                    l++;
-                    r--;
-
-                    // 从左边跳过重复的元素
-                    while (l < r && nums[l - 1] == nums[l]) {
-                        l++;
+                    //继续往中间找
+                    left++;
+                    right--;
+                    // 从左边跳过重复的元素  下面这些索引主要是因为上面执行过left++ right--
+                    //++ left也会变化的 除非写成 int x = left++;这个时候x是用的left没加之前的值
+                    while (left < right && nums[left - 1] == nums[left]) {
+                        left++;
                     }
-
                     // 从右边跳过重复的元素
-                    while (l < r && nums[r] == nums[r + 1]) {
-                        r--;
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
                     }
                 } else if (num < 0) {
-                    // num小于0说明左边太小
-                    // l向右移动
-                    l++;
+                    left++;
                 } else {
-                    // num大于0说明左边太大
-                    // r向左移动
-                    r--;
+                    right--;
                 }
+
             }
         }
 
